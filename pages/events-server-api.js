ea5b04e675424f82da0b6332c87c784a4667213e
Danielsan-80 <countzero80@gmail.com>
2023-03-10 14:55:25 +0100

    next-mongo-test-2

---
+++ pages/events-server-api.js
@@ -0,0 +1,49 @@
+
+
+// export const getServerSideProps = async (context)=>{
+//     const res = await fetch('http://localhost:3000/api/all-events')
+//     const events = await res.json()
+
+//     return {
+//         props: {
+//             data: events
+//         }
+//     }
+
+// }
+
+export const getServerSideProps = async(context) => {
+    try {
+        const devUrl = process.env.NEXT_PUBLIC_URL
+        const res = await fetch(devUrl + '/api/all-events');
+        const events = await res.json()
+    
+
+        return {
+            props: {
+                data: events
+            }
+        }
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const EventsServerApi = ({data}) => {
+  return (
+
+    <div>
+    {data.map(event=> (
+        <li key={event._id}>
+            <h1>{event.title}</h1>
+            <p>{event.city}</p>
+            <p>{event.description}</p>
+        </li>
+    ))}
+    
+    </div>
+  )
+}
+
+export default EventsServerApi
---
+++ pages/events-static-api.js
@@ -0,0 +1,50 @@
+
+
+// export const getServerSideProps = async (context)=>{
+//     const res = await fetch('http://localhost:3000/api/all-events')
+//     const events = await res.json()
+
+//     return {
+//         props: {
+//             data: events
+//         }
+//     }
+
+// }
+
+export const getStaticProps = async(context) => {
+
+    const devUrl = process.env.NEXT_PUBLIC_URL
+    try {
+        const res = await fetch(devUrl + '/api/all-events');
+        const events = await res.json()
+    
+
+        return {
+            props: {
+                data: events
+            }
+        }
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const EventsStaticApi = ({data}) => {
+  return (
+
+    <div>
+    {data.map(event=> (
+        <li key={event._id}>
+            <h1>{event.title}</h1>
+            <p>{event.city}</p>
+            <p>{event.description}</p>
+        </li>
+    ))}
+    
+    </div>
+  )
+}
+
+export default EventsStaticApi
